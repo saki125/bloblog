4ff14123e715606f8b9f9e3274994a86929914ae
chachamal <saki.takahashi@di-v.co.jp>
2020-12-29 18:12:02 +0900

    バリデーション、エラーメッセージの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+
+    validates :nickname,presence: true,uniqueness: { case_sensitive: true }
+
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数字混合で入力してください。'
+
+    
 end
