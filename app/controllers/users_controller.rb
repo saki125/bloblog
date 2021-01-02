82e02393d0a726126a19e02d0c42570bc4dd6404
chachamal <saki.takahashi@di-v.co.jp>
2021-01-02 17:19:42 +0900

    マイページの...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
     @posts = current_user.posts
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+           redirect_to root_path
+    end
+  end
+  
+  def update
+    if current_user.update(user_params)
+          redirect_to user_path(current_user)
+    else
+           redirect_to edit_user_path(current_user)
+    end
   end
 end
