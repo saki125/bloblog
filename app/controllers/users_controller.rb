88f83814881c92ea2a3b1d98a574ab2e7013f173
chachamal <saki.takahashi@di-v.co.jp>
2021-01-08 20:49:50 +0900

    編集後保存機能とマイページのCSS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,17 @@ class UsersController < ApplicationController
   end
   
   def update
-    if current_user.update(user_params)
-          redirect_to user_path(current_user)
+    @user = User.find(params[:id])
+    if @user.update!(user_params)
+          redirect_to user_path(@user)
     else
-           redirect_to edit_user_path(current_user)
+          render :edit
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname,:introduction)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
     validates :nickname,presence: true,uniqueness: { case_sensitive: true }
 
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数字混合で入力してください。'
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'は半角英数字混合で入力してください。', on: :create
 
     
 end
