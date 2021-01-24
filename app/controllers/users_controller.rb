501507d9b1858c41532907f1d816c7534e37ffbb
chachamal <saki.takahashi@di-v.co.jp>
2021-01-24 18:07:34 +0900

    削除の流れ（ルーティング、コントローラー、モデル）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,16 @@
 class UsersController < ApplicationController
+  before_action :set_user, only:[:edit, :update, :destroy]
+  before_action :if_user, only:[:edit, :update, :destroy]
+
   def show
     @posts = current_user.posts.order(created_at: :desc)
   end
 
   def edit
-    @user = User.find(params[:id])
-    if @user != current_user
-           redirect_to root_path
-    end
+    
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
           redirect_to user_path(@user)
     else
@@ -19,9 +18,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def destory
+    if @user.destroy 
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:nickname,:introduction,:image)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def if_user
+    if current_user.id != @user.id
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :posts
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :destroy]
 end
