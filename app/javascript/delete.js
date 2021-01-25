4d6f328282fa4c44aee18ce377bf96665e9c98e1
Hide-Morishita <hide.morishita@di-v.co.jp>
2021-01-25 18:25:14 +0900

    削除機能実装

---
+++ app/javascript/delete.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const postDelete = document.getElementById("post-delete")
+  postDelete.addEventListener("click", () => {
+    if (confirm("本当に削除しますか？") == true) {
+      const deleteStatus = true;
+      const deleteHtml = `<input value=${deleteStatus} type="hidden" name='post_delete'>`;
+      postDelete.insertAdjacentHTML('beforeend',deleteHtml )
+    } else {
+      alert("削除をキャンセルしました");
+    }
+  })
+})
